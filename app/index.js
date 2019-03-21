2cdc50776bf79861025821a6d14b24a82aae098f
Jon Winton <winton.jon@gmail.com>
2019-03-21 16:47:48 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,34 @@ const express = require('express'),
   app = express(),
   port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  console.log(JSON.stringify({
+    v: 1,
+    name: "songbird",
+    msg: "Hello World",
+    beat: {
+      name: "songbird01",
+      hostname: "songbird01",
+      version: "6.4.2"
+    },
+    level: 30,
+    route: "http://someurl.com/path/foo.html?query=string",
+    _label: "INFO",
+    hostname: "songbird01",
+    prospector: {
+      type: "log"
+    },
+    host: {
+      name: "songbird01"
+    },
+    file: "/var/lib/some/file/index.js",
+    type: "html",
+    input: {
+      type: "log"
+    }
+  }));
+  res.send('Hello World!');
+})
 app.get('/_status', (req, res) => res.status(204).send())
 
 app.listen(port, () => console.log(`Songbird is listening on port ${port}!`))
